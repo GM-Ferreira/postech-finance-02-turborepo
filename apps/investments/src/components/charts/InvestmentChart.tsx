9d75cfc96ab6d548db839f4cfa44dfe8f078c707
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2025-09-30 15:45:16 -0300

    feat: enhance InvestmentChart with active shape rendering and color customization

--- apps/investments/src/components/charts/InvestmentChart.tsx
+++ apps/investments/src/components/charts/InvestmentChart.tsx
@@ -1,21 +1,120 @@
 "use client";
 
-import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Legend,
+  ResponsiveContainer,
+  Sector,
+} from "recharts";
+import { PieSectorDataItem } from "recharts/types/polar/Pie";
 
 const data = [
-  { name: "Fundos de investimento", value: 15000 },
-  { name: "Tesouro Direto", value: 10000 },
-  { name: "Previdência Privada", value: 8000 },
-  { name: "Bolsa de Valores", value: 17000 },
+  {
+    name: "Fundos de investimento",
+    value: 15000,
+    abbreviation: "FI",
+    color: "#0088FE",
+  },
+  {
+    name: "Tesouro Direto",
+    value: 10000,
+    abbreviation: "TD",
+    color: "#8884d8",
+  },
+  {
+    name: "Previdência Privada",
+    value: 8000,
+    abbreviation: "PP",
+    color: "#FF8042",
+  },
+  {
+    name: "Bolsa de Valores",
+    value: 17000,
+    abbreviation: "BV",
+    color: "#FFBB28",
+  },
 ];
 
-const COLORS = ["#0088FE", "#8884d8", "#FF8042", "#FFBB28"];
+const renderActiveShape = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  startAngle,
+  endAngle,
+  fill,
+  payload,
+  percent,
+  value,
+}: PieSectorDataItem) => {
+  const RADIAN = Math.PI / 180;
+  const sin = Math.sin(-RADIAN * (midAngle ?? 1));
+  const cos = Math.cos(-RADIAN * (midAngle ?? 1));
+  const sx = (cx ?? 0) + ((outerRadius ?? 0) + 10) * cos;
+  const sy = (cy ?? 0) + ((outerRadius ?? 0) + 10) * sin;
+  const mx = (cx ?? 0) + ((outerRadius ?? 0) + 30) * cos;
+  const my = (cy ?? 0) + ((outerRadius ?? 0) + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.abbreviation}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={(outerRadius ?? 0) + 6}
+        outerRadius={(outerRadius ?? 0) + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`R$ ${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`(${((percent ?? 1) * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
 
 export const InvestmentChart = () => {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <PieChart>
         <Pie
+          activeShape={renderActiveShape}
           data={data}
           cx="50%"
           cy="50%"
@@ -27,7 +126,7 @@ export const InvestmentChart = () => {
           cornerRadius={15}
         >
           {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            <Cell key={`cell-${index}`} fill={entry.color} />
           ))}
         </Pie>
         <Legend
